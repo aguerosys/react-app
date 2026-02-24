73cc6e8dccbf8f8930d07821a0ce315341fcabd8
Brian Ezequiel Aguero <aguerolinkk@gmail.com>
2026-02-24 14:32:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
-function App() {
+
+const Description = () => {
+  return <p>Esta es una aplicación de React</p>;
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message color='red' msg='Estamos trabajando' />
+      <Message color='blue' msg='en un curso' />
+      <Message color='green' msg='de react' />
+      
+      <Description />
     </div>
-  );
+  )
 }
 
 export default App;
